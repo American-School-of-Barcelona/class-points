3f9291facd54e1e86c94e71aebbab1c870e9bd0e
BouncingElf10 <mathisbonne09@gmail.com>
2025-11-04 18:19:17 +0100

    added template dashboard layout

---
+++ frontend/app/components/DashboardLayout.vue
@@ -0,0 +1,87 @@
+<script setup>
+import WidgetCard from './WidgetCard.vue'
+</script>
+
+<template>
+    <div class="app">
+        <div class="container">
+            <div class="header">
+                <h1>Dashboard</h1>
+            </div>
+            <div class="grid">
+                <WidgetCard title="yo gurt 1">
+                    <p>lorem ipsum or sum like that</p>
+                </WidgetCard>
+
+                <WidgetCard title="yo gurt 2">
+                    <p>lorem ipsum or sum like that</p>
+                </WidgetCard>
+
+                <WidgetCard title="yo gurt 3">
+                    <p>lorem ipsum or sum like that</p>
+                </WidgetCard>
+
+                <WidgetCard title="wide yo gurt" class="wide">
+                    <p>lorem ipsum or sum like that BUT WIDDDDDDDDDDDDDDDDDDDDEEEEEEEEEEEEEEEEEEEEEEEEEEEE</p>
+                </WidgetCard>
+                <WidgetCard title="yo gurt 4">
+                    <p>lorem ipsum or sum like that</p>
+                </WidgetCard>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+body {
+    margin: 0;
+    background-color: var(--color-bg);
+    font-family: 'Inter', sans-serif;
+    height: 100vh;
+}
+
+.app {
+    height: 100vh;
+    background-color: var(--color-bg);
+    display: flex;
+    justify-content: center;
+    align-items: stretch;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: calc(100vh - 4em);
+    border-radius: calc(10px + 2em);
+    margin: 2em;
+    padding: 2em;
+    background-color: var(--color-bg-alt);
+    box-sizing: border-box;
+}
+
+.header {
+    margin-bottom: 1rem;
+    color: var(--color-text);
+}
+
+.header h1 {
+    margin: 0;
+    font-size: 1.8rem;
+    color: var(--color-accent);
+}
+
+.grid {
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: 1fr;
+    gap: 1rem;
+    width: 100%;
+}
+
+.grid .wide {
+    grid-column: span 2;
+}
+</style>
---
+++ frontend/app/components/WidgetCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+    title: String
+})
+</script>
+
+<template>
+    <div class="widget-card">
+        <h2 class="widget-title">{{ title }}</h2>
+        <div class="widget-body">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.widget-card {
+    background-color: var(--color-surface);
+    border-radius: 10px;
+    padding: 1rem;
+    color: var(--color-text);
+    display: flex;
+    flex-direction: column;
+}
+
+.widget-title {
+    font-size: 1.5rem;
+    color: var(--color-text);
+    margin: 0;
+}
+
+.widget-body {
+    font-size: 0.95rem;
+    line-height: 1.4;
+}
+</style>
