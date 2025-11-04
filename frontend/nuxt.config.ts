3efee11414f3640898caf833e34fdcd289a132a8
talracasb <tarachmel@asbarcelona.com>
2025-11-04 21:23:43 +0100

    chore(frontend): remove unused modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,14 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
-  css: ['@/assets/css/colors.css'],
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+  css: ['@/assets/css/colors.css']
 })
